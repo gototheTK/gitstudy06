11ad15aabce0dd2ad15f8623db4c6fcafd197191
user <user@domain.com>
2024-06-09 18:59:04 +0900

    function working

--- Branch.java
+++ Branch.java
@@ -1,5 +1,6 @@
 public class Branch {
     public static void main(String[] args) {
         System.out.println("Branch");
+        System.out.println("복제된 gitstudy06_clone의 function 브랜치를 수정합니다.");
     }
 }
